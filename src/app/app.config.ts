0a0432841c3b1c4a4e107a47161e198c28f429e5
ayyubmujawar1141 <ayyubrmujawar@gmail.com>
2026-01-28 10:33:34 +0530

    Server Side Rendering is enabled

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './interceptor/token/token-interceptor';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(withInterceptors([tokenInterceptor])),
-    provideRouter(routes)
+    provideRouter(routes), provideClientHydration(withEventReplay())
   ]
 };
